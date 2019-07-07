3cf2a1cc99e7da7393faff067bd136bd99b6bab7
Minsoo Kim <leios76@gmail.com>
2019-07-07 18:05:28 +0900

    최종 layer 배포 규칙 적용

--- module.sh
+++ module.sh
@@ -7,5 +7,5 @@ rm -f ${NAME}-modules.zip
 cd src && \
     zip -r ../${NAME}-modules.zip node_modules && \
     cd .. && \
-    LAYER_VERSION=$(aws lambda publish-layer-version --layer-name ${NAME}-modules --zip-file fileb://${NAME}-modules.zip | jq -r .LayerVersionArn) && \
+    LAYER_VERSION=$(aws lambda publish-layer-version --layer-name ${NAME}-modules --zip-file fileb://${NAME}-modules.zip --compatible-runtimes nodejs10.x | jq -r .LayerVersionArn) && \
     aws lambda update-function-configuration --function-name ${NAME} --layers ${LAYER_VERSION}
